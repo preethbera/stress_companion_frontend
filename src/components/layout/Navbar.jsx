0eec88060d940812fff2d7cd5ad8012fca0471cf
Preeth Bera <preethbera@gmail.com>
2026-02-01 15:45:39 +0530

    minor fix

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -201,13 +201,16 @@ export function Navbar({ user = null, onLogout }) {
 
               <DropdownMenuContent className="w-56" align="end" forceMount>
                 <DropdownMenuLabel className="font-normal">
-                  <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">
-                      {user.name}
-                    </p>
-                    <p className="text-xs leading-none text-muted-foreground">
-                      {user.email}
-                    </p>
+                  <div className="flex items-center gap-4">
+                    <UserAvatar user={user}  />
+                    <div className="flex flex-col space-y-1.5">
+                      <p className="text-sm font-medium leading-none">
+                        {user.name}
+                      </p>
+                      <p className="text-xs leading-none text-muted-foreground">
+                        {user.email}
+                      </p>
+                    </div>
                   </div>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
